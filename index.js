b8274a356fb832fe168ae7c167b5c6cd2efb4965
parth thakor <parththakor2610@gmail.com>
2023-07-16 18:44:56 +0530

    QuerySelector

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 
-// some li donot have the class list-group-item they don't affected by this exicution
 // let listItems = document.getElementsByClassName("list-group-item")
-// console.log(listItems)
+// let items = document.getElementsByTagName("li")
+let listItems2= document.querySelector(".list-group-item:nth-child(2)")
+let listItems3= document.querySelector(".list-group-item:nth-child(3)")
+listItems2.style.background = "green"
+// listItems3.style.display = "none"
+listItems3.style.visibility = "hidden"
+let items = document.getElementsByTagName("li")
+console.log(listItems3)
 // console.log(listItems[0])
 //  for(let i =0 ;i<listItems.length;i++){
 
 //    listItems[i].style.background = "#f4f4f4"
 //   }
-
-
-
-/// because of the li tagname its automatically get the all li from the dom and chages are refactes in to all li 
-// let items = document.getElementsByTagName("li")
 // console.log(items)
 // for(let i = 0 ; i<items.length ; i++){
 //   items[i].style.background = "#f4f4f4"
